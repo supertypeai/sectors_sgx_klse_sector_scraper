13408b59e22df47d670ee251153fb714e1d0ead9
Gulilil <juan.csantoso@gmail.com>
2024-08-30 15:46:07 +0700

    test

--- sector_scraper_main.py
+++ sector_scraper_main.py
@@ -109,12 +109,11 @@ if __name__ == "__main__":
     i3 = 3 * i1
 
     if (params == "SG"):
-      # p1 = Process(target=scrap_function_sg, args=(symbol_list[:i1], 1))
-      # p2 = Process(target=scrap_function_sg, args=(symbol_list[i1:i2], 2))
-      # p3 = Process(target=scrap_function_sg, args=(symbol_list[i2:i3], 3))
-      # p4 = Process(target=scrap_function_sg, args=(symbol_list[i3:], 4))
-      print(scrap_stock_page(BASE_URL, "569", "569"))
-      print(scrap_stock_page(BASE_URL, "594", "594"))
+      p1 = Process(target=scrap_function_sg, args=(symbol_list[:i1], 1))
+      p2 = Process(target=scrap_function_sg, args=(symbol_list[i1:i2], 2))
+      p3 = Process(target=scrap_function_sg, args=(symbol_list[i2:i3], 3))
+      p4 = Process(target=scrap_function_sg, args=(symbol_list[i3:], 4))
+
       
     else: # params == "MY"
       p1 = Process(target=scrap_function_my, args=(symbol_list[:i1], 1))
@@ -122,43 +121,43 @@ if __name__ == "__main__":
       p3 = Process(target=scrap_function_my, args=(symbol_list[i2:i3], 3))
       p4 = Process(target=scrap_function_my, args=(symbol_list[i3:], 4))
 
-  #   p1.start()
-  #   p2.start()
-  #   p3.start()
-  #   p4.start()
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
 
-  #   p1.join()
-  #   p2.join()
-  #   p3.join()
-  #   p4.join()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
 
-  #   # Handle null data
-  #   if (params == "SG"):
-  #     scrap_null_data_sg()
-  #   else: # params == "MY"
-  #     scrap_null_data_my()
+    # Handle null data
+    if (params == "SG"):
+      scrap_null_data_sg()
+    else: # params == "MY"
+      scrap_null_data_my()
 
-  #   # Merge data
-  #   df_final = combine_data(df_db_data, TYPE_ID, SYMBOL_COLUMN)
+    # Merge data
+    df_final = combine_data(df_db_data, TYPE_ID, SYMBOL_COLUMN)
 
-  #   # Convert to json. Remove the index in dataframe
-  #   records = df_final.to_dict(orient="records")
+    # Convert to json. Remove the index in dataframe
+    records = df_final.to_dict(orient="records")
 
-  #   # Upsert to db
-  #   try:
-  #     supabase.table(DB_TABLE).upsert(
-  #         records
-  #     ).execute()
-  #     print(
-  #         f"Successfully upserted {len(records)} data to database"
-  #     )
-  #   except Exception as e:
-  #     raise Exception(f"Error upserting to database: {e}")
+    # Upsert to db
+    try:
+      supabase.table(DB_TABLE).upsert(
+          records
+      ).execute()
+      print(
+          f"Successfully upserted {len(records)} data to database"
+      )
+    except Exception as e:
+      raise Exception(f"Error upserting to database: {e}")
 
-  #   # End time
-  #   end = time.time()
-  #   duration = int(end-start)
-  #   print(f"The execution time: {time.strftime('%H:%M:%S', time.gmtime(duration))}")
+    # End time
+    end = time.time()
+    duration = int(end-start)
+    print(f"The execution time: {time.strftime('%H:%M:%S', time.gmtime(duration))}")
 
-  # else:
-  #   print("[ERROR] Running argument is not complete")
+  else:
+    print("[ERROR] Running argument is not complete")
